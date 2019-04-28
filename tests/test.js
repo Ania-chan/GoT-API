c8542046be09bf096d66c3d40b731db6edf761ab
Ania-chan <ania.jurago@gmail.com>
2019-04-28 21:25:40 +0900

    wrote tests but they aren't working yet

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-const Promise = require("bluebird");
 const chai = require("chai");
 const expect = chai.expect;
 const chaiHttp = require("chai-http");
@@ -15,12 +14,13 @@ describe("GoT API", () => {
   let request;
   let JSONdata;
   let fakeKnex;
+  let database;
   beforeEach(() => {
     //setup
     request = chai.request(app).keepOpen();
-    const database = [
+    database = [
       {
-        characterName: "Aerys II Targaryen",
+        characterName: "Cersei_Lannister",
         houseName: "Targaryen",
         characterImageThumb: "https://m/imageThumb.jpg",
         characterImageFull: "https://m/imageFull.jpg",
@@ -31,21 +31,48 @@ describe("GoT API", () => {
     ];
 
     JSONdata = JSON.stringify(database);
-
-    //fakeKnex = sinon.stub(knex, "select").returns(JSONdata);
   });
   afterEach(() => {
     //teardown
     request.close();
     //fakeKnex.restore();
   });
-  it("should return list of characters", async () => {
-    expect(true).to.be.true;
-    console.log(1);
-    // const res = await request.get("/api/characters").then((hi) => "hi");
-    // console.log(res);
-    // //sinon.assert.calledOnce(fakeKnex);
-    // console.log(JSON.parse(res.text));
-    // JSON.parse(res.text).should.deep.equal(database);
+  describe("GET /api/characters", () => {
+    it("should return list of characters", async () => {
+      const res = await request.get("/api/characters").query({ limit: 2 });
+      JSON.parse(res.text).length.to.deep.equal(2);
+    });
+  });
+  describe("POST /api/characters", () => {
+    it("should add a new character by name", async () => {
+      const res = await request.post("/api/characters").send(database[0]);
+      const allCharacters = await request.get("/api/characters");
+      result[result.length - 1].should.deep.equal(database[0]);
+    });
+  });
+  describe("GET /api/characters/:name", () => {
+    it("should return a character page", async () => {
+      const res = await request.get("/api/pokemon/Cersei_Lannister");
+      res.should.be.json;
+      JSON.parse(res.text).should.not.be.undefined;
+      JSON.parse(res.text).should.deep.equal(database[0]);
+    });
+  });
+  describe("PATCH /api/characters/:name", () => {
+    it("should allow to change character's name", async () => {
+      const res = await request
+        .patch("/api/pokemon/Cersei_Lannister")
+        .query({ name: "test" });
+      res.should.be.json;
+      JSON.parse(res.text).should.not.be.undefined;
+      JSON.parse(res.text.name).should.equal("test");
+    });
+  });
+  describe("DELETE /api/characters/:name", () => {
+    it("should delete a character", async () => {
+      const res = await request.delete("/api/pokemon/Cersei_Lannister");
+      const expected = await request.get("/api/pokemon/Cersei_Lannister");
+      JSON.parse(expected.text).should.be.undefined;
+    });
   });
 });
